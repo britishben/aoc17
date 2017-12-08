86265b3d083d1f84f0dba38d653634f37f8d69b8
Benjamin P. McKenna <britishben@gmail.com>
2017-12-08 09:24:33 +0000

    Gofmt'd

--- day1/captcha/captcha.go
+++ day1/captcha/captcha.go
@@ -1,58 +1,58 @@
 package captcha
 
-import(
-    "strconv"
+import (
+	"strconv"
 )
 
-func IsEqual(a int,b int) (bool){
-    return a==b
+func IsEqual(a int, b int) bool {
+	return a == b
 }
 
-func CodeOne(s string) (value int){
-    a := Digits(s)
-    c := 0 //what to compare to v
+func CodeOne(s string) (value int) {
+	a := Digits(s)
+	c := 0 //what to compare to v
 
-    for i, v := range a{
+	for i, v := range a {
 
-        if i == len(a)-1 {
-            c=a[0]
-        }else{
-            c=a[i+1]
-        }
+		if i == len(a)-1 {
+			c = a[0]
+		} else {
+			c = a[i+1]
+		}
 
-        if IsEqual(v, c) {
-            value += v
-        }
-    }
-    return value
+		if IsEqual(v, c) {
+			value += v
+		}
+	}
+	return value
 }
 
-func CodeTwo(s string) (value int){
-    a := Digits(s)
-    c := 0 //what to compare to v
+func CodeTwo(s string) (value int) {
+	a := Digits(s)
+	c := 0 //what to compare to v
 
-    demi := len(a)/2
+	demi := len(a) / 2
 
-    for i, v := range a{
+	for i, v := range a {
 
-        if i >= demi {
-            c=a[i-demi]
-        }else{
-            c=a[i+demi]
-        }
+		if i >= demi {
+			c = a[i-demi]
+		} else {
+			c = a[i+demi]
+		}
 
-        if IsEqual(v, c) {
-            value += v
-        }
-    }
-    return value
+		if IsEqual(v, c) {
+			value += v
+		}
+	}
+	return value
 }
 
-func Digits(s string) (r []int){
-    //s := strconv.Itoa(i)
-    for _,v := range s {
-        n,_ := strconv.Atoi(string(v))
-        r = append (r, n)
-    }
-    return r
+func Digits(s string) (r []int) {
+	//s := strconv.Itoa(i)
+	for _, v := range s {
+		n, _ := strconv.Atoi(string(v))
+		r = append(r, n)
+	}
+	return r
 }
--- day1/main.go
+++ day1/main.go
@@ -1,20 +1,19 @@
 package main
 
-import(
-    "fmt"
-    "os"
-    "bufio"
-    "aoc17/day1/captcha"
+import (
+	"aoc17/day1/captcha"
+	"bufio"
+	"fmt"
+	"os"
 )
 
-func main(){
-    scanner := bufio.NewScanner(os.Stdin)
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
 
-    fmt.Println("Hello")
-    fmt.Println("Enter code:")
-    scanner.Scan()
-    ans := scanner.Text()
-    fmt.Printf("Answer one is: %d.\n", captcha.CodeOne(ans))
-    fmt.Printf("Answer two is: %d.\n", captcha.CodeTwo(ans))
+	fmt.Println("Hello")
+	fmt.Println("Enter code:")
+	scanner.Scan()
+	ans := scanner.Text()
+	fmt.Printf("Answer one is: %d.\n", captcha.CodeOne(ans))
+	fmt.Printf("Answer two is: %d.\n", captcha.CodeTwo(ans))
 }
-
